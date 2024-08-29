987c1c3ec9f855a1bab36e803bf184c988cbf888
yJoaoVhz <166820655+yJoaoVhz@users.noreply.github.com>
2024-08-29 08:04:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,51 @@
 const botoes  = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2024, 7,29);
-const tempoObjetivo2 = new Date(2024, 8,29);
-const tempoObjetivo3 = new Date(2024, 9,29);
-const tempoObjetivo4 = new Date(2024, 10,29);
-let segundos;
-let minutos;
-let horas;
-let dias;
+const tempoObjetivo1 = new Date(2024,11,24,23,59);
+const tempoObjetivo2 = new Date(2024,9,28,23,59);
+const tempoObjetivo3 = new Date(2025,0,1,23,59);
+const tempoObjetivo4 = new Date(2025,2,14,23,59);
+
+function calculaTempo(tempoObjetivo){
 const agora = new Date();
-segundos = (tempoObjetivo1 - agora)/1000;
-minutos = segundos /60;
-horas = minutos /60;
-dias = horas/24;
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor(horas);
-dias = Math.floor(dias);
-tempo[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos`
+let segundos
+let minutos
+let horas
+let dias
+
+segundos = Math.floor((tempoObjetivo-agora)/1000)
+minutos = Math.floor(segundos/60)
+horas = Math.floor(minutos/60)
+dias = Math.floor(horas/24)
+
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
+
+if (segundos >= 0){
+    return [dias,horas,minutos,segundos]
+}else{
+    return 'Prazo Finalizado !'
+}
+ 
+const tempoUm = calculaTempo(tempoObjetivo1)
+const tempoDois = calculaTempo(tempoObjetivo2)
+const tempoTres = calculaTempo(tempoObjetivo3)
+const tempoQuatro = calculaTempo(tempoObjetivo4)
+
+}
+
+tempo[0].textContent = calculaTempo(tempoObjetivo1)
+tempo[1].textContent = calculaTempo(tempoObjetivo2)
+tempo[2].textContent = calculaTempo(tempoObjetivo3)
+tempo[3].textContent = calculaTempo(tempoObjetivo4)
+
 console.log(tempo);
-console.log(botoes);
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
-        for (let j = 0;j < botoes.length; j++){
+        for (let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
